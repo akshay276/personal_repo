810bf49d9918dc8b586146cb86b857a068acecb3
akshay276 <akshay276@gmail.com>
2017-03-07 03:31:15 +0530

    HTTP Service with  RootScope
    Temp~~Delete this updated files

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-var app = angular.module('app', ['ui.grid', 'ui.grid.edit']);
+var app = angular.module('app', [ 'ngTouch', 'ui.grid' ]);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
-   $scope.data = [
-     { name: 'Bob', title: 'CEO' },
-         { name: 'Frank', title: 'Lowly Developer' }
-   ];
+app.controller('MainCtrl', function($scope, $rootScope, $http, $log) {
 
-   $scope.columnDefs = [
-     {name: 'name', enableCellEdit: true},
-     {name: 'title', enableCellEdit: true}
-   ];
- }]);
+	var _successCallback = function(data, status, headers) {
+
+		$scope.myArray = [];
+		$scope.myArray = data;
+		console.log("name is  : : ", $scope.myArray.data[1].name);
+
+		$rootScope.myNewArray = $scope.myArray;
+		console.log("New name is  : : ", $rootScope.myNewArray.data[1].name);
+	};
+	var _errorCallback = function(data, status, headers) {
+	};
+
+	$scope.UserAkshay = $http({
+		method : 'GET',
+		url : 'NgJsonServlet'
+	}).then(_successCallback, _errorCallback);
+
+	console.log("Last Line: : ", $rootScope.myNewArray);
+	$scope.data = [];
+
+	$scope.columnDefs = [ {
+		name : 'name',
+		enableCellEdit : true
+	}, {
+		name : 'title',
+		enableCellEdit : true
+	} ];
+
+})
+
+/*
+ * $scope.data = [ { name : 'Bob', title : 'CEO' }, { name : 'Frank', title :
+ * 'Lowly Developer' } ];
+ * 
+ * $scope.columnDefs = [ { name : 'name', enableCellEdit : true }, { name :
+ * 'title', enableCellEdit : true } ];
+ */
+
