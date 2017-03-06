a6bcc3be4942a995ab6b9163a3508816cfcb0f6f
akshay276 <akshay276@gmail.com>
2017-03-07 02:36:51 +0530

    Temp Code snippet for $HTTP service Angular

---
+++ NgJsonServlet.java
@@ -0,0 +1,31 @@
+package com.cts.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NgJsonServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public NgJsonServlet() {
+		System.out.println("Inside Servlet Constructor");
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("Inside Servlet");
+
+		String json = "  {\"col1\":\"blah\",\"col2\":\"blah\"}   ";
+		System.out.println(json);
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().write(json);
+
+	}
+
+}
